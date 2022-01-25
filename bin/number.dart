2ef3fe272a196510287d6e63f5fc9aa9e19539dc
apelbeku <syauqyalhaq5@gmail.com>
2022-01-25 10:57:50 +0700

    data type number

---
+++ bin/number.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Data type number 2 = int (bulat) & double (desimal)
+
+  int number1 = 10;
+  double number2 = 10.5;
+
+  // num can be used for int & double
+  num number3 = 10;
+
+  print(number1);
+  print(number2);
+  print(number3);
+
+  number3 = 10.5;
+  print(number3);
+}
