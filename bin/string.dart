8812774b480a04673424bdeceead022434294cc1
apelbeku <syauqyalhaq5@gmail.com>
2022-01-25 11:20:21 +0700

    data type string

--- bin/string.dart
+++ bin/string.dart
@@ -2,6 +2,23 @@ void main() {
   String firstName = "Just";
   String lastName = 'Luck1';
 
+  // string interpolation
   String fullName = '$firstName ${lastName}';
-  print(fullName); 
+  print(fullName);
+
+  // backslash for ingnoring expression
+  var text = 'this is \'dart\' \$cool';
+  print(text);
+
+  // combine string
+  var name1 = firstName + lastName;
+  var name2 = 'Just' 'Luck1';
+  print(name1);
+  print(name2);
+
+  var longString = '''
+  this is multiline string
+  ''';
+  print(longString);
+  
 }
