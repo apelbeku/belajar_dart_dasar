3da443c372b8c5182c0aece1e7f095d3f2666eb4
apelbeku <syauqyalhaq5@gmail.com>
2022-01-26 10:22:55 +0700

    type test operator

---
+++ bin/type_test_operator.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  dynamic variable = 100;
+
+  var variableInt = variable as int;
+  print(variableInt);
+
+  print(variableInt is int);
+  print(variableInt is bool);
+  print(variableInt is String);
+
+  print(variableInt is! int);
+  print(variableInt is! bool);
+  print(variableInt is! String);
+
+
+}
