2c9e1c34cc48f3e58d9e110ddd3cb797fb00b747
apelbeku <syauqyalhaq5@gmail.com>
2022-01-26 11:34:29 +0700

    if else

---
+++ bin/if_else.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  var score = 80;
+  var attendance = 80;
+
+  if (score >= 80 && attendance >= 80) {
+    print('you are graduate');
+  } else if (score >= 80 && attendance >=  75) {
+    print('you are graduate');
+  } else if (score >= 75 && attendance >=  70) {
+    print('you are graduate');
+  } else if (score >= 70 && attendance >=  60) {
+    print('you are graduate');
+  } else {
+    print('you are not graduate');
+  };
+  
+}
