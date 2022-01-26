19fbe16c125a616f337659f725b9f5e08bca1dbd
apelbeku <syauqyalhaq5@gmail.com>
2022-01-26 11:24:32 +0700

    symbol

---
+++ bin/symbol.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  /// rarely used data types. Cannot be space # can space Symbol('text)
+  Symbol symbol1 = Symbol("Belajar Dart");
+  var symbol2 = #belajar;
+
+  print(symbol1);
+  print(symbol2);
+
+}
