5588c997697d6a40cbe26ea821e532eebcf20f37
apelbeku <syauqyalhaq5@gmail.com>
2022-01-26 10:01:14 +0700

    augmented assigment

---
+++ bin/augmented_assigment.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  /// Augmented Assigment
+  /**
+   * a = a + 10;
+   * a += 10;
+   * a -= 10;
+   * a *= 10;
+   * a /= 10;
+   * a ~/= 10;
+   * a %= 10;
+   *  */
+  var a = 10;
+  a += 10;
+
+  print(a);
+
+  /// Increment & Decrement
+  var i = 0;
+  var j = i++; // j = i; i++
+  var k = ++i; // k = k++; i
+
+  print(j);
+  print(k);
+}
