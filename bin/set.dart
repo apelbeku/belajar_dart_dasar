4c44a9ad811306bccac0dd2611612b0ea5811351
apelbeku <syauqyalhaq5@gmail.com>
2022-01-26 11:06:39 +0700

    set

---
+++ bin/set.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  /// Set ignoring duplicate, irregular and have no index
+  /// create Set
+  Set<int> numbers = {};
+  var doubles = <double>{};
+  var strings = <String>{};
+
+  print(numbers);
+
+  /// directly declare Set
+  var names = {
+    'Dunk',
+    'Write'
+    'kick',
+  };
+
+  print(names);
+
+  /// Manipulating Set
+  /// .add()
+  /// .length()
+  /// .remote()
+
+}
